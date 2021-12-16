9e776a2f6ae1649e2946dc4feddfbc936efb86a7
tsuk2108 <t_kaneshiro@outlook.com>
2021-12-16 13:50:48 +0900

    add: lookahead

--- lexer/lexer.c
+++ lexer/lexer.c
@@ -19,31 +19,116 @@ static void	set_flag(unsigned char *flags, const size_t bit)
 	(*flags) |= bit;
 }
 
-static bool	lookahead(char ch, unsigned char *flags)
+static void	unset_flag(unsigned char *flags, const size_t bit)
 {
-	/* Returns true if the character constitutes a token */
-	/* Implemented later */
+	(*flags) &= ~bit;
+}
+
+static bool	is_special_char(const char ch)
+{
+	if (ch == '\'' || ch == '\"' || ch == '$' || ch == EOF
+		|| ch == '\0' || ch == '<' || ch == '>' || ch == '|')
+		return (true);
+	else
+		return (false);
+}
+
+static bool	lookahead_first_char(const char ch, unsigned char *flags)
+{
+	unset_flag(flags, FIRST_FLAG);
+	if (ch == '\'')
+		set_flag(flags, QUOTE_FLAG);
+	else if (ch == '\"')
+		set_flag(flags, DOUBLE_QUOTE_FLAG);
+	else if (ch == '$')
+		set_flag(flags, VARIABLE_FLAG);
+	else if (ch == '<' || ch == '>')
+		set_flag(flags, REDIRECT_FLAG);
+	else if (ch == '|')
+		set_flag(flags, PIPE_FLAG);
+	else if (ch == EOF || ch == '\0')
+		set_flag(flags, EOF_FLAG);
+	else
+		return (false);
 	return (true);
 }
 
-char	*get_token(const char *line, size_t *index)
+static bool	lookahead(char ch, unsigned char *flags)
 {
+	if (check_flag((*flags), FIRST_FLAG))
+		lookahead_first_char(ch, flags);
+	else if (check_flag((*flags), QUOTE_FLAG))
+	{
+		if (ch == '\'')
+			return (false);
+	}
+	else if (check_flag((*flags), DOUBLE_QUOTE_FLAG))
+	{
+		if (ch == '\"')
+			return (false);
+	}
+	else if (check_flag((*flags), REDIRECT_FLAG))
+		return (false);
+	else if (check_flag((*flags), PIPE_FLAG))
+		return (false);
+	else
+	{
+		if (ft_isspace(ch) || is_special_char(ch))
+			return (false);
+	}
+	if (check_flag((*flags), EOF_FLAG))
+		return (false);
+	return (true);
+}
+
+static bool check_last(const char *line, unsigned char *flags,
+		size_t *index, size_t *length)
+{
+	if (check_flag((*flags), QUOTE_FLAG)
+		|| check_flag((*flags), DOUBLE_QUOTE_FLAG))
+	{
+		(*index) += 1;
+		(*length) += 1;
+	}	
+	if (check_flag((*flags), REDIRECT_FLAG))
+	{
+		if (line[(*length)] == line[((*length) - 1)])
+		{
+			(*index) += 1;
+			(*length) += 1;
+		}
+	}
+	if (check_flag((*flags), EOF_FLAG))
+		return (true);
+	return (false);
+}
+
+char	*get_token(const char *line, size_t *index, size_t size)
+{
+	size_t			start;
 	size_t			length;
 	unsigned char	flags;
 	char			*token;
 
-	while (ft_isspace(line[(*index)]))
+	start = 0;
+	while (ft_isspace(line[start]))
+	{
 		(*index) += 1;
-	length = 0;
+		start += 1;
+	}
+	if (((*index)) >= size)
+		return (NULL);
+	length = start;
 	flags = '\0';
-	while (lookahead(line[(*index)], &flags))
+	set_flag(&flags, FIRST_FLAG);
+	while (lookahead(line[length], &flags))
 	{
 		(*index) += 1;
 		length += 1;
 	}
-	if (check_flag(flags, EOF_FLAG) && check_flag(NO_TOKEN_FLAG))
+	if (check_last(line, &flags, index, &length))
 		return (NULL);
-	token = ft_substr(line, ((*index) - length), length);
+	token = ft_substr(line, start, (length - start));
 	return (token);
 }
 
@@ -56,11 +141,12 @@ t_list	*lexer(const char *line)
 
 	index = 0;
 	token_list = ft_lstnew("");
-	while (1)
+	while (line[index] != '\0' && line[index] != EOF)
 	{
-		token = get_token(&(line[index]), &index);
+		token = get_token(&(line[index]), &index, ft_strlen(line));
 		if (token == NULL)
 			break ;
+		printf("[%s]\n", token);
 		current = ft_lstnew(token);
 		if (current == NULL)
 		{
@@ -68,7 +154,24 @@ t_list	*lexer(const char *line)
 			return (NULL);
 		}
 		ft_lstadd_back(&token_list, current);
-		index += ft_strlen(token);
 	}
 	return (token_list);
 }
+
+/*
+int main()
+{
+	t_list	*token_list;
+	t_list	*tmp;
+	char 	*str = "mkdir test| echo \"create test\">>log.txt";
+
+	token_list = lexer(str);
+	while (token_list != NULL)
+	{
+		tmp = token_list->next;
+		free(token_list);
+		token_list = tmp;
+	}
+	return (0);
+}
+*/
--- lexer/lexer.h
+++ lexer/lexer.h
@@ -2,10 +2,16 @@
 # define LEXER_H
 
 # include <stdbool.h>
+# include <stdio.h>
 # include "libft.h"
 
-# define EOF_FLAG       1
-# define NO_TOKEN_FLAG  2
+# define QUOTE_FLAG         1
+# define DOUBLE_QUOTE_FLAG  2
+# define VARIABLE_FLAG      4
+# define REDIRECT_FLAG      8
+# define PIPE_FLAG          16
+# define EOF_FLAG           32
+# define FIRST_FLAG         64
 
 t_list	*lexer(const char *line);
 
