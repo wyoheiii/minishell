9445f6b60e5cd675221d6d8cbc9d0a22de0683db
tsuk2108 <t_kaneshiro@outlook.com>
2022-02-05 21:21:38 +0900

    add

--- includes/parser.h
+++ includes/parser.h
@@ -6,28 +6,15 @@
 
 # include "libft.h"
 # include "delimiter.h"
+# include "redirect_list.h"
 # include "utils.h"
-
-# define NONE 			    0
-# define PIPE 			    1
-# define REDIRECT_INPUT	    2
-# define REDIRECT_OUTPUT	3
-# define REDIRECT_APPEND	4
-# define HERE_DOCUMENT	    5
-
-typedef struct s_redirect
-{
-	char			*filename;
-	int				state;
-	struct s_redirect	*next;
-}	t_redirect;
 
 typedef struct s_parsed
 {
-	char			**command;
-	int				state;
-	t_redirect		*redirect;
-	struct s_parsed	*next;
+	char				**command;
+	int					state;
+	struct s_redirect	*redirect;
+	struct s_parsed		*next;
 }	t_parsed;
 
 t_parsed	*parser(t_list **token_list);
