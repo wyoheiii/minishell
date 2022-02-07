bce8e5109509797bce0d185db830c5dfd82c13ed
tsuk2108 <t_kaneshiro@outlook.com>
2022-02-07 23:09:18 +0900

    add

--- srcs/expansion.c
+++ srcs/expansion.c
@@ -352,7 +352,7 @@ void    expansion(t_parsed *parsed, t_envlist *env)
 	new_command = (char **)malloc(sizeof(char *) * size);
 	size = 0;
 	tmp = argv_list;
-	while (tmp->argv != NULL)
+	while (tmp != NULL)
 	{
 		new_command[size] = tmp->argv;
 		size += 1;
