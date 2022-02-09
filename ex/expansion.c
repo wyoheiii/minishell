d6f8bdcb42b975184739d2bcd968272425c63dc5
tsuk2108 <t_kaneshiro@outlook.com>
2022-02-09 22:18:20 +0900

    add: split expansion.c

--- ex/expansion.c
+++ ex/expansion.c
@@ -37,60 +37,6 @@ static bool	check_func(t_expand *list, size_t index)
 	return (false);
 }
 
-char	*string_before_param(t_expand *list, char **line)
-{
-	char	*before_param;
-	char	*new_line;
-
-	if (list->index != list->checked)
-	{
-		before_param = ft_substr(list->argv, list->checked,
-				(list->index - list->checked));
-		if (before_param == NULL)
-		{
-			ft_putendl_fd("malloc failure", 2);
-			exit(EXIT_FAILURE);
-		}
-		new_line = ft_strjoin(*line, before_param);
-		free(*line);
-		free(before_param);
-		if (new_line == NULL)
-		{
-			ft_putendl_fd("malloc failure", 2);
-			exit(EXIT_FAILURE);
-		}
-		return (new_line);
-	}
-	return (*line);
-}
-
-char	*join_remaining_string(t_expand *list, char **line)
-{
-	char	*remaining;
-	char	*new_command;
-
-	if (list->checked != list->index)
-	{
-		remaining = ft_substr(
-				list->argv, list->checked, list->index - list->checked);
-		if (remaining == NULL)
-		{
-			ft_putendl_fd("malloc failure", 2);
-			exit(EXIT_FAILURE);
-		}
-		new_command = ft_strjoin(*line, remaining);
-		free(*line);
-		free(remaining);
-		if (new_command == NULL)
-		{
-			ft_putendl_fd("malloc failure", 2);
-			exit(EXIT_FAILURE);
-		}
-		return (new_command);
-	}
-	return (NULL);
-}
-
 //リスト内の文字列を展開する
 void	expand_argv(t_expand *list, t_envlist *envlist)
 {
